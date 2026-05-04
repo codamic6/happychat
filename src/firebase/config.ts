30130565583e6ed175b388c5b9f5104ac3265bd4
Muhammad Ahmad <itsmuhammadahmad06@gmail.com>
2026-05-04 15:40:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1550651103-f2622",
+  "appId": "1:352094486415:web:0c6618b8f19db405d815fa",
+  "apiKey": "AIzaSyCd_nTfgS8bDgG70DZQ1BlY0B0w4iVjgMY",
+  "authDomain": "studio-1550651103-f2622.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "352094486415"
+};
