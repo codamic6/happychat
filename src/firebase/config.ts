17926266ee262a22ad9430aec2d713871b8cbad3
Muhammad Ahmad <itsmuhammadahmad06@gmail.com>
2026-05-11 09:23:46 +0000

    save firebase config.ts file in .env export it to .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-1550651103-f2622",
-  "appId": "1:352094486415:web:0c6618b8f19db405d815fa",
-  "apiKey": "AIzaSyCd_nTfgS8bDgG70DZQ1BlY0B0w4iVjgMY",
-  "authDomain": "studio-1550651103-f2622.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "352094486415"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
