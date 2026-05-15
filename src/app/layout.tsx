14863d9279e2e3679c0762488a24d9f9327e1923
Muhammad Ahmad <itsmuhammadahmad06@gmail.com>
2026-05-15 09:26:53 +0000

    favicon not shjowong

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/mob.png" />
       </head>
       <body className="font-body antialiased selection:bg-primary selection:text-primary-foreground overflow-x-hidden">
